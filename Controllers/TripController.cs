9dd34d822687068fe61d6b898bf1c8706a521328
naomiOvadia <n0583267045@gmail.com>
2024-10-31 13:37:15 +0200

    update

--- Controllers/TripController.cs
+++ Controllers/TripController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using projectNaomi.model;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +10,60 @@ namespace projectNaomi.Controllers
     [ApiController]
     public class TripController : ControllerBase
     {
+
+        private static List<Trip> trips = new List<Trip>{
+                new Trip {code="1",location="bney brak",date=DateTime.Today,numRegisters=5, idGuide="123"},
+                new Trip {code="2",location="tel aviv",  date=DateTime.Today,numRegisters=8,idGuide ="123" }
+            };
+        public TripController()
+        {
+
+        }
+
         // GET: api/<TripController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Trip> Get()
         {
-            return new string[] { "value1", "value2" };
+            return trips;
         }
 
         // GET api/<TripController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Trip Get(int code)
         {
-            return "value";
+            var index = trips.FindIndex(e => e.code.Equals(code));
+            if (index != -1)
+                return trips[index];
+            return null;
         }
 
         // POST api/<TripController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Trip Post([FromBody] Trip trip)
         {
+            trips.Add(trip);
+            return trip;
         }
 
         // PUT api/<TripController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int code, [FromBody] Trip trip)
         {
+            var index = trips.FindIndex(e => e.code.Equals(code));
+            trips[index].code = trip.code;
+            trips[index].location =trip.location ;
+            trips[index].date =trip.date ;
+            trips[index].numRegisters =trip.numRegisters;
+            trips[index].idGuide =trip.idGuide;
         }
 
         // DELETE api/<TripController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(int code)
         {
+            var index = trips.FindIndex(e => e.code.Equals(code));
+            if(index != -1)
+                trips.RemoveAt(index);
         }
     }
 }
